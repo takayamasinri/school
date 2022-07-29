8b96c752f8d26d7db3c088ff4ba24d233d07e8aa
takayamasinri <takayamasinri@icloud.com>
2022-07-29 12:34:36 +0000

    [add]記事いtラン画面

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,6 +12,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/','PostController@index');
